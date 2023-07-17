ce93a553a326066c13977fa63c0a7b7beee3eb12
minh <minh.vh.1068@aptechlearning.edu.vn>
2023-07-18 00:27:51 +0700

    init clinet

--- Frontend/client/app/app.js
+++ Frontend/client/app/app.js
@@ -1,19 +1,9 @@
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.home',
 ])
   .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('');
-    $routeProvider
-      .when('/view1', {
-        templateUrl: 'homePage/home.html'
-      })
-    // .when('/view2', {
-    //   template: '<my-app-view2></my-app-view2>'
-    // })
-    // .otherwise({ redirectTo: '/' });
+
   }]);
 
 angular.module('myApp').component('app', {
