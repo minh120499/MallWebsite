76ba5971a1426ed574d750dc3187b80537fad14f
minh <minh.vh.1068@aptechlearning.edu.vn>
2023-08-08 01:21:40 +0700

    fix store

--- Frontend/admin/app/app.js
+++ Frontend/admin/app/app.js
@@ -79,17 +79,66 @@ angular.module('myApp').component('appHeader', {
     var dashboardState = {
       name: 'dashboard',
       url: '/',
-      template: '<div ng-click="toggleSidebar()">Dashboard</div>'
+      template: '<div>Dashboard</div>'
     }
 
     var bannersState = {
       name: 'banners',
       url: '/banners',
-      template: '<div ng-click="toggleSidebar()">Banners</div>'
+      template: '<div>Banners</div>'
+    }
+
+    var categoriesState = {
+      name: 'categories',
+      url: '/categories',
+      template: '<div>Categories</div>'
+    }
+
+    var storesState = {
+      name: 'stores',
+      url: '/stores',
+      template: '<div>Stores</div>'
+    }
+
+    var productsState = {
+      name: 'products',
+      url: '/products',
+      template: '<div>Products</div>'
+    }
+
+    var ordersState = {
+      name: 'orders',
+      url: '/orders',
+      template: '<div>Orders</div>'
+    }
+
+    var usersState = {
+      name: 'users',
+      url: '/users',
+      template: '<div>Users</div>'
+    }
+
+    var feedbacksState = {
+      name: 'feedbacks',
+      url: '/feedbacks',
+      template: '<div>Feedbacks</div>'
+    }
+
+    var settingsState = {
+      name: 'settings',
+      url: '/settings',
+      template: '<div>Settings</div>'
     }
 
     $stateProvider.state(dashboardState);
     $stateProvider.state(bannersState);
+    $stateProvider.state(categoriesState);
+    $stateProvider.state(storesState);
+    $stateProvider.state(productsState);
+    $stateProvider.state(ordersState);
+    $stateProvider.state(usersState);
+    $stateProvider.state(feedbacksState);
+    $stateProvider.state(settingsState);
   });
 
 angular.module('myApp').component('sideBar', {
