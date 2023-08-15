4742efe026d832ac231bb2a21868aaa26ae7e8e2
minh <minh.vh.1068@aptechlearning.edu.vn>
2023-08-15 20:24:30 +0700

    fix: header

--- Frontend/client/app/components/header/header.js
+++ Frontend/client/app/components/header/header.js
@@ -1,3 +1,12 @@
-export default angular.module('myApp').component('appHeader', {
-  templateUrl: 'components/header/header.html',
-});
+export default angular.module('myApp')
+  .component('appHeader', {
+    templateUrl: 'components/header/header.html',
+    controller: ['$scope', '$location', '$http', 'paginationService',
+      function PaginationController($scope, $location, $http, paginationService) {
+        $scope.page = $location.path().slice(1) || "home";
+
+        $scope.setPage = function (page) {
+          $scope.page = page
+        }
+      }]
+  });
