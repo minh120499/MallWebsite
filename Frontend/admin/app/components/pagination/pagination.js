d49aa81d59c5231bdf6eb5642acc0142a8a62a38
minh <minh.vh.1068@aptechlearning.edu.vn>
2023-08-19 10:12:41 +0700

    fix: clinet

--- Frontend/admin/app/components/pagination/pagination.js
+++ Frontend/admin/app/components/pagination/pagination.js
@@ -5,12 +5,25 @@ export default angular.module('myApp')
       $scope.total = paginationService.getTotal() || 0;
       $scope.page = paginationService.getPage() || 1;
       $scope.limit = paginationService.getLimit() || 10;
+      $scope.paginationMax = $scope.page === Math.ceil($scope.total / ($scope.limit || 10))
 
       $scope.pagination = Array.from({ length: Math.ceil($scope.total / ($scope.limit || 10)) }, (_, index) => ({
         page: index + 1,
         isActive: $scope.page === index + 1,
       }));
       $scope.paginationLength = $scope.pagination.length;
+
+      $scope.handlePrev = () => {
+        $scope.page = $scope.page - 1;
+        paginationService.setPage($scope.page);
+        $location.search('page', $scope.page);
+      }
+
+      $scope.handleNext = () => {
+        $scope.page = $scope.page + 1;
+        paginationService.setPage($scope.page);
+        $location.search('page', $scope.page);
+      }
 
       $scope.handlePageClick = function (page) {
         paginationService.setPage(page);
--- Frontend/client/app/components/pagination/pagination.js
+++ Frontend/client/app/components/pagination/pagination.js
@@ -5,6 +5,7 @@ export default angular.module('myApp')
       $scope.total = paginationService.getTotal() || 0;
       $scope.page = paginationService.getPage() || 1;
       $scope.limit = paginationService.getLimit() || 10;
+      $scope.paginationMax = $scope.page === Math.ceil($scope.total / ($scope.limit || 10))
 
       $scope.pagination = Array.from({ length: Math.ceil($scope.total / ($scope.limit || 10)) }, (_, index) => ({
         page: index + 1,
@@ -12,9 +13,24 @@ export default angular.module('myApp')
       }));
       $scope.paginationLength = $scope.pagination.length;
 
+      $scope.handlePrev = () => {
+        $scope.page = $scope.page - 1;
+        paginationService.setPage($scope.page);
+        $location.search('page', $scope.page);
+        window.scrollTo(0, 630)
+      }
+
+      $scope.handleNext = () => {
+        $scope.page = $scope.page + 1;
+        paginationService.setPage($scope.page);
+        $location.search('page', $scope.page);
+        window.scrollTo(0, 630)
+      }
+
       $scope.handlePageClick = function (page) {
         paginationService.setPage(page);
         $location.search('page', page);
+        window.scrollTo(0, 630)
       };
 
       $scope.handleLimitClick = function (limit) {
@@ -27,6 +43,7 @@ export default angular.module('myApp')
           paginationService.setLimit(limit);
           $location.search('limit', limit);
         }
+        window.scrollTo(0, 630)
       };
     }]
   });
